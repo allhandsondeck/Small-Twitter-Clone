86b66ca31b0d834a061862f945b63a1ababee0aa
allhandsondeck <utkukopuz.nl@outlook.com>
2023-10-15 12:35:58 +0200

    refactor: move filter logic from tweet list component into filter service

--- src/app/components/tweet-list/tweet-list.component.ts
+++ src/app/components/tweet-list/tweet-list.component.ts
@@ -6,7 +6,6 @@ import {
   OnInit,
 } from '@angular/core';
 import { Subject, combineLatest, of, switchMap, takeUntil } from 'rxjs';
-import { VisibilityType } from 'src/app/enums/visibility-type.enum';
 import { Tweet } from 'src/app/models/tweet';
 import { DataService } from 'src/app/services/data.service';
 import { FilterService } from 'src/app/services/filter.service';
@@ -23,7 +22,7 @@ export class TweetListComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataService: DataService,
-    private FilterService: FilterService,
+    private filterService: FilterService,
     private cd: ChangeDetectorRef
   ) {}
 
@@ -35,10 +34,10 @@ export class TweetListComponent implements OnInit, OnDestroy {
         switchMap((tweets) => {
           return combineLatest([
             of(tweets),
-            this.FilterService.searchKeywordSubject,
-            this.FilterService.visibilityTypeSubject,
-            this.FilterService.currentPageSubject,
-            this.FilterService.tweetsPerPageSubject,
+            this.filterService.searchKeywordSubject,
+            this.filterService.visibilityTypeSubject,
+            this.filterService.currentPageSubject,
+            this.filterService.tweetsPerPageSubject,
           ]);
         })
       )
@@ -50,74 +49,17 @@ export class TweetListComponent implements OnInit, OnDestroy {
           newPage,
           newTweetsPerPage,
         ]) => {
-          const searchKeyword = newSearchKeyword;
-          const currentPage = newPage;
-          const tweetsPerPage = newTweetsPerPage;
-          const tweetsFilteredByHandleOrName =
-            this.getTweetsFilteredByHandleOrName(searchKeyword, tweets);
-          const tweetsFilteredByVisibilityType =
-            this.getTweetsFilteredByVisibilityType(
-              newVisibilityType,
-              tweetsFilteredByHandleOrName
-            );
-          this.tweets = this.getTweetsFilteredByPagination(
-            tweetsFilteredByVisibilityType,
-            currentPage,
-            tweetsPerPage
+          this.tweets = this.filterService.filterTweets(
+            tweets,
+            newSearchKeyword,
+            newVisibilityType,
+            newPage,
+            newTweetsPerPage
           );
-          this.setTotalPageCount(
-            tweetsFilteredByVisibilityType.length,
-            tweetsPerPage
-          );
+
           this.cd.detectChanges();
         }
       );
-  }
-
-  private getTweetsFilteredByHandleOrName(
-    searchKeyword: string,
-    tweets: Tweet[]
-  ) {
-    return tweets.filter(
-      (tweet) =>
-        tweet.user.name.toLowerCase().includes(searchKeyword.toLowerCase()) ||
-        tweet.user.screen_name
-          .toLowerCase()
-          .includes(searchKeyword.toLowerCase())
-    );
-  }
-
-  private getTweetsFilteredByVisibilityType(
-    visibilityType: VisibilityType,
-    tweets: Tweet[]
-  ): Tweet[] {
-    switch (visibilityType) {
-      case VisibilityType.AllTweets:
-        return tweets;
-
-      case VisibilityType.RetweetedTweets:
-        return tweets?.filter((tweet) => tweet.retweeted_status);
-    }
-  }
-
-  private setTotalPageCount(
-    tweetsFilteredByVisibilityTypeLength: number,
-    tweetsPerPage: number
-  ) {
-    const totalPageCount = Math.ceil(
-      tweetsFilteredByVisibilityTypeLength / tweetsPerPage
-    );
-    this.FilterService.totalPageCountSubject.next(totalPageCount);
-  }
-
-  private getTweetsFilteredByPagination(
-    tweetsFilteredByVisibilityType: Tweet[],
-    currentPage: number,
-    tweetsPerPage: number
-  ): Tweet[] {
-    const startIndex = (currentPage - 1) * tweetsPerPage;
-    const endIndex = startIndex + tweetsPerPage;
-    return tweetsFilteredByVisibilityType.slice(startIndex, endIndex);
   }
 
   ngOnDestroy() {
--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { VisibilityType } from '../enums/visibility-type.enum';
+import { Tweet } from '../models/tweet';
 
 @Injectable({
   providedIn: 'root',
@@ -16,4 +17,75 @@ export class FilterService {
     5
   );
   totalPageCountSubject: Subject<number> = new Subject<number>();
+
+  filterTweets(
+    tweets: Tweet[],
+    searchKeyword: string,
+    visibilityType: VisibilityType,
+    currentPage: number,
+    tweetsPerPage: number
+  ): Tweet[] {
+    const tweetsFilteredByHandleOrName = this.filterByHandleOrName(
+      searchKeyword,
+      tweets
+    );
+    const tweetsFilteredByVisibilityType = this.filterByVisibilityType(
+      visibilityType,
+      tweetsFilteredByHandleOrName
+    );
+
+    this.setTotalPageCount(
+      tweetsFilteredByVisibilityType.length,
+      tweetsPerPage
+    );
+
+    return this.filterByPagination(
+      tweetsFilteredByVisibilityType,
+      currentPage,
+      tweetsPerPage
+    );
+  }
+
+  filterByHandleOrName(searchKeyword: string, tweets: Tweet[]): Tweet[] {
+    return tweets.filter(
+      (tweet) =>
+        tweet.user.name.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+        tweet.user.screen_name
+          .toLowerCase()
+          .includes(searchKeyword.toLowerCase())
+    );
+  }
+
+  filterByVisibilityType(
+    visibilityType: VisibilityType,
+    tweets: Tweet[]
+  ): Tweet[] {
+    switch (visibilityType) {
+      case VisibilityType.AllTweets:
+        return tweets;
+
+      case VisibilityType.RetweetedTweets:
+        return tweets?.filter((tweet) => tweet.retweeted_status);
+    }
+  }
+
+  setTotalPageCount(
+    tweetsFilteredByVisibilityTypeLength: number,
+    tweetsPerPage: number
+  ) {
+    const totalPageCount = Math.ceil(
+      tweetsFilteredByVisibilityTypeLength / tweetsPerPage
+    );
+    this.totalPageCountSubject.next(totalPageCount);
+  }
+
+  filterByPagination(
+    tweets: Tweet[],
+    currentPage: number,
+    tweetsPerPage: number
+  ): Tweet[] {
+    const startIndex = (currentPage - 1) * tweetsPerPage;
+    const endIndex = startIndex + tweetsPerPage;
+    return tweets.slice(startIndex, endIndex);
+  }
 }
